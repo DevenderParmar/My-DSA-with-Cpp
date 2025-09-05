5716b4bd4cc46084a34f686bdcb2c50d7ccebfbf
DevenderParmar <devparmar2906@gmail.com>
2025-09-05 19:25:49 +0530

    added prime number logic

---
+++ BASIC_CPP/17_prime.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+bool isprime( int n )
+{
+    for(int i = 2;i*i<= n;i++)
+    {
+        if(n%i == 0)
+        {
+            return false;
+        }
+
+
+    }
+    return true;
+}
+int main()
+{
+    int n ;
+    cout<< "enter a number = ";
+    cin>> n;
+
+    if(isprime(n))
+    {
+        cout<< "PRIME";
+    }
+    else  
+    {
+        
+        cout<<"NOT PRIME";
+    }
+ return 0;
+}
