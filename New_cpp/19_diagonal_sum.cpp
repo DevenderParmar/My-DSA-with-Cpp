b16f74af98f0f91d361b2a733a769198d833913c
DevenderParmar <devparmar2906@gmail.com>
2025-09-22 19:13:42 +0530

    2d matrix

---
+++ New_cpp/19_diagonal_sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int diagonal_sum(int matrix[][3],int n)
+{
+    int sum = 0;
+    for(int i = 0;i<n;i++)
+    {
+        sum+= matrix[i][i];
+        if(i != n-i-)
+
+    }
+
+}
+int main()
+{
+    return 0;
+}
