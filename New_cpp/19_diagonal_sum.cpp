502431aed42b945b41d4a4e0395ec06505d97011
DevenderParmar <devparmar2906@gmail.com>
2025-09-26 23:06:38 +0530

    solved new medium

--- New_cpp/19_diagonal_sum.cpp
+++ New_cpp/19_diagonal_sum.cpp
@@ -1,17 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int diagonal_sum(int matrix[][3],int n)
-{
+
+int diagonal_sum(int matrix[][3], int n) {
     int sum = 0;
-    for(int i = 0;i<n;i++)
-    {
-        sum+= matrix[i][i];
-        if(i != n-i-)
-
+    for (int i = 0; i < n; i++) {
+        sum += matrix[i][i]; // primary diagonal
+        if (i != n - i - 1)  // avoid double-counting center for odd n
+            sum += matrix[i][n - i - 1]; // secondary diagonal
     }
-
+    return sum;
 }
-int main()
-{
+
+int main() {
+    int matrix[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    
+    int sum = diagonal_sum(matrix, 3);
+    cout << "Diagonal sum: " << sum << endl;
+
     return 0;
 }
