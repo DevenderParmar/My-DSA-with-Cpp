46a1000cbb49e09b3f5c3dfd063cd951bd4a6da5
DevenderParmar <devparmar2906@gmail.com>
2025-10-01 20:21:38 +0530

    Remove tracked exe files

--- 02_Medium_cpp/13_2nd_rotated_search.cpp
+++ 02_Medium_cpp/13_2nd_rotated_search.cpp
@@ -2,52 +2,51 @@
 #include <vector>
 using namespace std;
 
-bool search(vector<int>& nums, int target) {
+bool search(vector<int> &nums, int target)
+{
     int start = 0, end = nums.size() - 1;
-    while (start <= end) {
+    while (start <= end)
+    {
         int mid = start + (end - start) / 2;
-        if (nums[mid] == target) {
+        if (nums[mid] == target)
             return true;
-        }
-        if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
+
+        if (nums[start] == nums[mid] && nums[mid] == nums[end])
+        {
             start++;
             end--;
-        } else if (nums[start] <= nums[mid]) {
-            if (target >= nums[start] && target < nums[mid]) {
+        }
+        else if (nums[start] <= nums[mid])
+        {
+            if (target >= nums[start] && target < nums[mid])
                 end = mid - 1;
-            } else {
+            else
                 start = mid + 1;
-            }
-        } else {
-            if (target > nums[mid] && target <= nums[end]) {
+        }
+        else
+        {
+            if (target > nums[mid] && target <= nums[end])
                 start = mid + 1;
-            } else {
+            else
                 end = mid - 1;
-            }
         }
     }
     return false;
 }
 
-int main() {
-    int n, target;
-    cout << "Enter the size of the array: ";
-    cin >> n;
+int main()
+{
+    vector<int> nums = {2, 5, 6, 0, 0, 1, 2};  // rotated sorted array with duplicates
+    int target = 0;
 
-    vector<int> nums(n);
-    cout << "Enter the elements of the array:\n";
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
-    }
+    cout << "Array: ";
+    for (int x : nums) cout << x << " ";
+    cout << "\nTarget: " << target << endl;
 
-    cout << "Enter the target to search: ";
-    cin >> target;
-
-    if (search(nums, target)) {
+    if (search(nums, target))
         cout << "Target found in the array.\n";
-    } else {
+    else
         cout << "Target not found in the array.\n";
-    }
 
     return 0;
 }
