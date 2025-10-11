8fb16927a280ae7151cb9fa691607cdbb478153d
DevenderParmar <devparmar2906@gmail.com>
2025-10-12 00:47:14 +0530

    updated file

--- New_cpp/22_recursion.cpp
+++ New_cpp/22_recursion.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 using namespace std;
-void printnum( int n)
+void printnum(int n)
 {
-    if(n == 1){
-        cout<< 1;
+    if (n == 1)
+    {
+        cout << 1;
         return;
     }
-    cout<<n<<" ";
-    printnum(n-1);
+    cout << n << " ";
+    printnum(n - 1);
+}
+int fact(int n)
+{
+    if (n == 0)
+        return 1;
+    return fact(n - 1) * n;
+}
+int sum(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    return n + sum(n - 1);
 }
 int main()
 {
-    int  n = 10;
+    int n = 10;
     printnum(n);
-
-
+    cout << endl;
+    cout << fact(5);
+    cout << endl;
+    cout << sum(n);
 
     return 0;
 }
