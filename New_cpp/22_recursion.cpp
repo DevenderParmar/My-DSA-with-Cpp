8a283ffb2a5354fc0261974d97d5ab929c9bbfae
DevenderParmar <devparmar2906@gmail.com>
2025-10-13 23:34:42 +0530

    recursion concepts

--- New_cpp/22_recursion.cpp
+++ New_cpp/22_recursion.cpp
@@ -24,6 +24,15 @@ int sum(int n)
     }
     return n + sum(n - 1);
 }
+
+bool issorted(int arr[],int n )
+{
+    if(n == 0 || n == 1)
+    {
+        return true;
+    }
+    return arr[n-1]>= arr[n-2] && issorted(arr,n-1);
+}
 int main()
 {
     int n = 10;
@@ -32,6 +41,8 @@ int main()
     cout << fact(5);
     cout << endl;
     cout << sum(n);
+    int arr[5] = {1,2,3,4,5};
+    cout<<endl<< issorted(arr,5);
 
     return 0;
 }
